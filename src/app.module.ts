45af6556edc576bdf8730ce02fb3c17df0e1802a
João Paulo <jpjoao1001@gmail.com>
2021-04-02 18:51:11 -0300

    configured typeorm and made get post and put routes to contacts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [ContactsModule],
+  imports: [
+    ContactsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/contacts/contacts/contacts.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { identity } from 'rxjs';
+import { Contact } from '../contact.entity';
+import { ContactsService } from './contacts.service';
+
+@Controller('contacts')
+export class ContactsController {
+  constructor(private contactsService: ContactsService) {}
+
+  @Get()
+  index(): Promise<Contact[]> {
+    return this.contactsService.findAll();
+  }
+
+  @Post('create')
+  async create(@Body() contactData: Contact): Promise<any> {
+    return this.contactsService.create(contactData);
+  }
+
+  @Put(':id/update')
+  async update(
+    @Param('id') id: number,
+    @Body() contactData: Contact,
+  ): Promise<any> {
+    contactData.id = Number(id);
+    console.log('Update # ', contactData.id);
+    return this.contactsService.update(contactData);
+  }
+}
